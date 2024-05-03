095564261ca04fdddf5a49be2555a0d7f477d499
febin123 <febsep96@gmail.com>
2024-05-03 21:30:08 +0530

    added 3D computers into Hero Compoenents

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,11 +16,11 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I\m <span className='text-[#915EFF]'>Adrian</span>
+            Hi, Im <span className='text-[#915EFF]'>Febin</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I develop Frontend web applications and user <br className='sm:block hidden' />
+            interfaces designs
           </p>
         </div>
       </div>
