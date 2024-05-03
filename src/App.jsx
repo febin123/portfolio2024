76c581d4c03a1645fc721c84857650ae4f513f0a
febin123 <febsep96@gmail.com>
2024-05-03 21:25:54 +0530

    added Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
+import { BrowserRouter } from "react-router-dom";
 
-import { BrowserRouter } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/Navbar'
+import {Hero, Navbar } from "./components";
 
-function App() {
-
-
+const App = () => {
   return (
     <BrowserRouter>
-    <div className='relative z-0 bg-primary'>
-      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-        <Navbar/>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero/>
+        
+        </div>
       </div>
-
-    </div>
-  </BrowserRouter>
-
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
