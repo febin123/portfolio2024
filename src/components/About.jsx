f8748a809f8ba149fa3b1f9d1afee2b57fe5beb3
Febin Skariya <31155921+febin123@users.noreply.github.com>
2025-08-07 17:15:13 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -50,7 +50,7 @@ const About = () => {
         HTML5, CSS3 and expertise in frameworks like React, and
         Three.js. I'm a quick learner and collaborate closely with clients to
         create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        real-world problems. Let's work together to bring your ideas to life!!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
